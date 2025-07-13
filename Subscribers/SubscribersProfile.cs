ebfb7d2de79ca54acbcd612d446f87d57f1ce255
emirjongorrica <gorricaemirjon@gmail.com>
2025-07-13 21:55:39 +0200

    Created controller: SubscribersController

---
+++ Subscribers/SubscribersProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ParkingLotManagement.Models;
+
+namespace ParkingLotManagement.Subscribers
+{
+    public class SubscribersProfile : Profile
+    {
+        public SubscribersProfile()
+        {
+            CreateMap<Subscriber, SubscribersWebDto>().ReverseMap();
+        }
+    }
+}
---
+++ Subscribers/SubscribersWebDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingLotManagement.Subscribers
+{
+    public class SubscribersWebDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string IdCardNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public string PlateNumber { get; set; }
+    }
+}
